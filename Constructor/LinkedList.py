af630ce52397c238c79ead8c635ccd49631618b4
Pawan Pahune <pawanpahune@Pawans-MacBook-Air.local>
2025-12-27 00:27:54 +0530

    DSA Repository in Python

---
+++ Constructor/LinkedList.py
@@ -0,0 +1,68 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+    
+    def print_list(self):
+        temp= self.head
+        while temp:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def prepend(self,value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+    
+    def pop(self):
+        if self.head is None:
+            return None
+        pre = self.head
+        temp = self.head
+        while temp.next:
+            pre = temp
+            temp = temp.next
+        self.tail=pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+    
+    def pop_first(self):
+        if self.head is None:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+            self.head = None
+        return temp
+
+
